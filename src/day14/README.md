2c014d9424866a72cb8dd32a95353b1d74e34364
Norbert Kiesel <nk@iname.com>
2024-12-13 23:22:16 -0800

    day 14

---
+++ src/day14/README.md
@@ -0,0 +1,11 @@
+# 🎄 Advent of Code 2024 - day 14 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2024/day/14)
+
+## Notes
+
+Again pretty simple. However, the terminating condition for part 2 was stolen by reading some
+discussions on Slack. Some other criteria could have been to have at lest one horizontal line
+of 10 points or more, or having a 1, 3, 5, 7 sequence of lines for the tree tip.
---
+++ src/day14/index.ts
@@ -0,0 +1,89 @@
+import run from "aocrunner"
+import { parseInt } from "lodash-es"
+
+type Robot = {
+  px: number
+  py: number
+  vx: number
+  vy: number
+}
+
+const parseInput = (rawInput: string): Robot[] => {
+  return rawInput.split("\n").map((l) => {
+    const d = l.match(/p=(\d+),(\d+) v=(-?\d+),(-?\d+)/)
+    return {
+      px: parseInt(d[1]),
+      py: parseInt(d[2]),
+      vx: parseInt(d[3]),
+      vy: parseInt(d[4]),
+    }
+  })
+}
+
+const part1 = (rawInput: string) => {
+  const robots = parseInput(rawInput)
+  const [wx, wy] = robots.length < 15 ? [11, 7] : [101, 103]
+  for (let i = 0; i < 100; i++) {
+    robots.forEach((r) => {
+      r.px = (r.px + r.vx + wx) % wx
+      r.py = (r.py + r.vy + wy) % wy
+    })
+  }
+  const count = [0, 0, 0, 0]
+  const [bx, by] = [Math.floor(wx / 2), Math.floor(wy / 2)]
+  robots.forEach((r) => {
+    if (r.px < bx && r.py < by) count[0]++
+    if (r.px > bx && r.py < by) count[1]++
+    if (r.px < bx && r.py > by) count[2]++
+    if (r.px > bx && r.py > by) count[3]++
+  })
+  return count[0] * count[1] * count[2] * count[3]
+}
+
+const part2 = (rawInput: string) => {
+  const robots = parseInput(rawInput)
+  const [wx, wy] = robots.length < 15 ? [11, 7] : [101, 103]
+  for (let i = 1; true; i++) {
+    const s = new Set<string>()
+    robots.forEach((r) => {
+      r.px = (r.px + r.vx + wx) % wx
+      r.py = (r.py + r.vy + wy) % wy
+      s.add(`${r.px},${r.py}`)
+    })
+    if (s.size === robots.length) return i
+  }
+}
+
+const sample1 = `
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+`
+
+run({
+  part1: {
+    tests: [
+      {
+        input: sample1,
+        expected: 12,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+})
